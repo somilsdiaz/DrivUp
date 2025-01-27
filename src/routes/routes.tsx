7fead187a8588aaa46e0736bd46e53efa14b94c2
Jcoll05 <jcolla@uninorte.edu.co>
2025-01-27 00:06:44 -0500

    Added login page, changed website icon to the project's icon.

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,7 +1,12 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Login from '../pages/login';
 import Inicio from '../pages/inicio';
 
 const router = createBrowserRouter([
+    {
+        path: "/login", // Ruta para el login
+        element: <Login />,
+    },
     {
         path:"/",
         element: <Inicio />,
