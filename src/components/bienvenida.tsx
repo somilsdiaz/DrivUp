5120dde1c5694c63a5b234e9ca01cd2575bddc1b
Jcoll05 <jcolla@uninorte.edu.co>
2025-02-06 19:15:45 -0500

    Added first implementation for this feature.

--- src/components/bienvenida.tsx
+++ src/components/bienvenida.tsx
@@ -4,7 +4,7 @@ import fleets from '../assets/Merged-fleets.jpg';
 const Bienvenida: React.FC = () => {
 
     return (
-        <body className="h-auto min-w-screen flex justify-start items-center bg-[url('src/assets/Merged-fleets.jpg')] bg-auto bg-cover bg-no-repeat bg-center bg-black/50 bg-blend-multiply h-65">
+        <body className="h-auto flex justify-start items-center bg-[url('src/assets/Merged-fleets.jpg')] bg-auto bg-cover bg-no-repeat bg-center bg-black/50 bg-blend-multiply h-65 overflow-hidden">
             <div className="mt-3 mb-3 ml-8 w-full max-w-2xl bg-[#e8e8e8] p-8 shadow-lg rounded-lg border-2 border">
                 <h1 className="text-[#003747] text-4xl font-bold">UniBus</h1>
 
@@ -16,7 +16,7 @@ const Bienvenida: React.FC = () => {
                 mejorando la experiencia de transporte.
                 </p>
 
-                <button className="mt-6 px-6 py-3 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-600 transition-all">
+                <button className="mt-6 px-6 py-3 bg-[#2e9700] text-white font-semibold rounded-lg shadow-md hover:bg-[#38761d] transition-all">
                     Solicitar Ruta
                 </button>
 
