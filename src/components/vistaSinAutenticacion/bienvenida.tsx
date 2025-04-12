a00c565b888027418184a574cf4406144811a9fc
somilsdiaz <somil.sandoval@icloud.com>
2025-04-12 15:12:09 -0500

    fix[Header]: Elements of the header has been updated and the files have been reoganized

---
+++ src/components/vistaSinAutenticacion/bienvenida.tsx
@@ -0,0 +1,18 @@
+//import { useNavigate } from "react-router-dom";
+
+
+
+const Bienvenida: React.FC = () => {
+
+    //const navigate = useNavigate();
+
+    return (
+        <body
+         className="relative py-80 w-full flex sm:justify-center md:justify-center lg:justify-start items-center h-65 overflow-hidden">
+
+        </body>
+
+    );
+}
+
+export default Bienvenida;
--- src/pages/inicio.tsx
+++ src/pages/inicio.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import HeaderFooter from '../layouts/headerFooter';
-import Bienvenida from '../components/bienvenida';
+import Bienvenida from '../components/vistaSinAutenticacion/bienvenida';
 const Inicio: React.FC = () => {
     return (
         <HeaderFooter>
