c37ffc242580c97fd5e32082be63ca9ca62929d5
Julian Almario <juliandavidalmario@gmail.com>
2025-04-13 22:10:37 -0500

    Finish

---
+++ src/components/vistaSinAutenticacion/llamadaAccion.tsx
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+
+export function LlamadaAccion(){
+    return(
+        <section className="flex flex-col gap-4 justify-center p-4 text-center">
+            <h2 className="text-2xl font-bold text-[#4A4E69]">Únete a DrivUp hoy mismo</h2>
+            <p>Ya sea que necesites un viaje o quieras
+            generar ingresos como conductor, DrivUp es tu mejor opción.</p>
+            <div className="flex gap-2 justify-center items-center">
+                <button className="p-2 text-[#4A4E69] font-bold bg-[#F2B134] rounded-sm cursor-pointer hover:bg-[#F2B134]/90 transition-all hover:transform">Descarga la app</button>
+                <button className="p-2 "><Link to="/como-funciona-">Conocer Mas</Link></button>
+                </div>
+        </section>
+    );
+}
