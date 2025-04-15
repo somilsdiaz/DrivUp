75058281a7eb0e33e243990604bbc15caa647b4a
somilsdiaz <somil.sandoval@icloud.com>
2025-04-15 11:44:50 -0500

    feat[HU006-01]: delete elements unused (videocall and voicecall icons) of the chatmessage component

--- src/components/RutasProgramadas/chatMessage.tsx
+++ src/components/RutasProgramadas/chatMessage.tsx
@@ -76,16 +76,7 @@ const ChatMessage: React.FC<ChatMessageProps> = ({
                     </div>
                 </div>
                 <div className="flex space-x-2">
-                    <button className="p-2 rounded-full hover:bg-white/10 transition-colors" title="Llamada de voz">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
-                        </svg>
-                    </button>
-                    <button className="p-2 rounded-full hover:bg-white/10 transition-colors" title="Videollamada">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 10l4.553-2.276A1 1 0 0121 8.618v6.764a1 1 0 01-1.447.894L15 14M5 18h8a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v8a2 2 0 002 2z" />
-                        </svg>
-                    </button>
+
                     <button className="p-2 rounded-full hover:bg-white/10 transition-colors" title="Más opciones">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z" />
