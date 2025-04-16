aaa89ca207fdc13d0859de66fbb02afd07e23a52
Julian Almario <juliandavidalmario@gmail.com>
2025-04-16 12:52:46 -0500

    Margin and padding fixed

--- src/components/vistaSinAutenticacion/llamadaAccion.tsx
+++ src/components/vistaSinAutenticacion/llamadaAccion.tsx
@@ -2,8 +2,8 @@ import { Link } from "react-router-dom";
 
 export function LlamadaAccion(){
     return(
-        <section className="flex flex-col gap-4 justify-center p-4 text-center">
-            <h2 className="text-2xl font-bold text-[#4A4E69]">Únete a DrivUp hoy mismo</h2>
+        <section className="flex flex-col gap-4 justify-center p-4 text-center bg-[#2D5DA1] text-white">
+            <h2 className="text-2xl font-bold ">Únete a DrivUp hoy mismo</h2>
             <p>Ya sea que necesites un viaje o quieras
             generar ingresos como conductor, DrivUp es tu mejor opción.</p>
             <div className="flex gap-2 justify-center items-center">
